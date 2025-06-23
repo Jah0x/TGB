2e1c830234063b2e96170cacf0555722871fc2e5
Jah0x <petra12231@gmail.com>
2025-06-23 16:41:29 +0300

    Update config.py

--- telegram_accounting_bot/config.py
+++ telegram_accounting_bot/config.py
@@ -6,5 +6,5 @@ ACCOUNTING_CHAT_ID = int(os.getenv("ACCOUNTING_CHAT_ID", "-1002242266701"))
 ACCOUNTING_TOPIC_ID = int(os.getenv("ACCOUNTING_TOPIC_ID", "4294967301"))
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "6480599695"))               # ID администратора
 DB_PATH = os.getenv("DB_PATH", "accounting.db")
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "7311112662:AAEL_xnNZKxPxGAzSJGnKeSPivf551HqHzU")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "7873432803:AAGs3Zlk_1sZEFHZ-qIOyvvAHoz7Wncyeiw")
 
