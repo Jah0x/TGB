3892a2b7b739e37aa40a65952108453a38c49251
Jah0x <petra12231@gmail.com>
2025-06-23 17:26:48 +0300

    Update config.py

--- telegram_accounting_bot/config.py
+++ telegram_accounting_bot/config.py
@@ -1,9 +1,7 @@
 import os
 
-# ID темы/чата "БУХГАЛТЕРИЯ" в которой учитываются продажи
-ACCOUNTING_CHAT_ID = int(os.getenv("ACCOUNTING_CHAT_ID", "4294967301"))
-# ID конкретной темы (thread) форума
-ACCOUNTING_TOPIC_ID = int(os.getenv("ACCOUNTING_TOPIC_ID", "-1002242266701"))
+ACCOUNTING_CHAT_ID = int(os.getenv("ACCOUNTING_CHAT_ID", "-1002242266701"))
+ACCOUNTING_TOPIC_ID = int(os.getenv("ACCOUNTING_TOPIC_ID", "5"))
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "6480599695"))               # ID администратора
 DB_PATH = os.getenv("DB_PATH", "accounting.db")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "7873432803:AAGs3Zlk_1sZEFHZ-qIOyvvAHoz7Wncyeiw")
