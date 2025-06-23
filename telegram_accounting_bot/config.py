0fa11baa7f444192fc9ff636c7d9c2e14e040610
Jah0x <petra12231@gmail.com>
2025-06-23 17:22:01 +0300

    Update config.py

--- telegram_accounting_bot/config.py
+++ telegram_accounting_bot/config.py
@@ -1,14 +1,13 @@
 import os
 
 # ID темы/чата "БУХГАЛТЕРИЯ" в которой учитываются продажи
-ACCOUNTING_CHAT_ID = int(os.getenv("ACCOUNTING_CHAT_ID", "-1002242266701"))
+ACCOUNTING_CHAT_ID = int(os.getenv("ACCOUNTING_CHAT_ID", "4294967301"))
 # ID конкретной темы (thread) форума
-ACCOUNTING_TOPIC_ID = int(os.getenv("ACCOUNTING_TOPIC_ID", "4294967301"))
+ACCOUNTING_TOPIC_ID = int(os.getenv("ACCOUNTING_TOPIC_ID", "-1002242266701"))
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "6480599695"))               # ID администратора
 DB_PATH = os.getenv("DB_PATH", "accounting.db")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "7873432803:AAGs3Zlk_1sZEFHZ-qIOyvvAHoz7Wncyeiw")
 
 # Включает отправку администратору сведений о каждой сохранённой продаже.
 # Используется только во время тестирования.
-SEND_EACH_SALE = bool(int(os.getenv("SEND_EACH_SALE", "0")))
 
