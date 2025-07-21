d7a3d11cf57dcb429a829a5c6c2d5d969b568a35
Jah0x <petra12231@gmail.com>
2025-07-21 22:36:50 +0300

    Update config.py

--- telegram_accounting_bot/config.py
+++ telegram_accounting_bot/config.py
@@ -4,7 +4,7 @@ ACCOUNTING_CHAT_ID = int(os.getenv("ACCOUNTING_CHAT_ID", "-1002242266701"))
 ACCOUNTING_TOPIC_ID = int(os.getenv("ACCOUNTING_TOPIC_ID", "5"))
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "6480599695"))               # ID администратора
 DB_PATH = os.getenv("DB_PATH", "accounting.db")
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "7873432803:AAGs3Zlk_1sZEFHZ-qIOyvvAHoz7Wncyeiw")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "
 
 # Включает отправку администратору сведений о каждой сохранённой продаже.
 # Используется только во время тестирования.
